fa36bd7a22ed5e60a5dfea5c89c175cd602f7b67
“Patrick <podonnell105@outlook.com>
2025-07-06 00:31:21 +0100

    ppp

--- test_failing.py
+++ test_failing.py
@@ -1,9 +1,8 @@
-def failing_function():
-    return 1 / 0  # This will cause division by zero
+def add(a, b):
+    return a + b
 
-def test_failing():
-    assert failing_function() == 0  # This test will fail
-    print("This should never print")
-def test_division_by_zero():
-    result = 1 / 0
-    assert result == 0
+def test_add():
+    assert add(2, 3) == 5  # This passes
+    assert add(5, 3) == 2  # This FAILS - should be 8
+    assert add(0, 0) == 0  # This passesffffff
+    ffffffffff6666
