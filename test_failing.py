197319c95e217b87a75ca884e9b7f896730f886a
“Patrick <podonnell105@outlook.com>
2025-07-05 21:18:37 +0100

    Add more failing tests

--- test_failing.py
+++ test_failing.py
@@ -4,3 +4,6 @@ def failing_function():
 def test_failing():
     assert failing_function() == 0  # This test will fail
     print("This should never print")
+def test_division_by_zero():
+    result = 1 / 0
+    assert result == 0
